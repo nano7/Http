c82d21b4f58edab47b9e679887f63d5495fd81ad
Bruno Goncalves <bugotech@gmail.com>
2018-06-15 05:41:15 -0300

    add flash in session

--- src/Session.php
+++ src/Session.php
@@ -5,6 +5,18 @@ use Nano7\Foundation\Support\Arr;
 class Session extends \Symfony\Component\HttpFoundation\Session\Session
 {
     /**
+     * Save the session data to storage.
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        $this->ageFlashData();
+
+        parent::save();
+    }
+
+    /**
      * Determine if the session contains old input.
      *
      * @param  string  $key
@@ -27,5 +39,157 @@ class Session extends \Symfony\Component\HttpFoundation\Session\Session
     public function getOldInput($key = null, $default = null)
     {
         return Arr::get($this->get('_old_input', []), $key, $default);
+    }
+
+    /**
+     * Put a key / value pair or array of key / value pairs in the session.
+     *
+     * @param  string|array  $key
+     * @param  mixed       $value
+     * @return void
+     */
+    public function put($key, $value = null)
+    {
+        if (! is_array($key)) {
+            $key = [$key => $value];
+        }
+
+        foreach ($key as $arrayKey => $arrayValue) {
+            $this->set($arrayKey, $arrayValue);
+        }
+    }
+
+    /**
+     * Push a value onto a session array.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function push($key, $value)
+    {
+        $array = $this->get($key, []);
+
+        $array[] = $value;
+
+        $this->put($key, $array);
+    }
+
+    /**
+     * Remove keys.
+     *
+     * @param $keys
+     * @return void
+     */
+    public function forget($keys)
+    {
+        $keys = (array) $keys;
+        foreach ($keys as $key) {
+            $this->remove($key);
+        }
+    }
+
+    /**
+     * Age the flash data for the session.
+     *
+     * @return void
+     */
+    public function ageFlashData()
+    {
+        $this->forget($this->get('_flash.old', []));
+
+        $this->put('_flash.old', $this->get('_flash.new', []));
+
+        $this->put('_flash.new', []);
+    }
+
+    /**
+     * Flash a key / value pair to the session.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function flash($key, $value = true)
+    {
+        $this->put($key, $value);
+
+        $this->push('_flash.new', $key);
+
+        $this->removeFromOldFlashData([$key]);
+    }
+
+    /**
+     * Flash a key / value pair to the session for immediate use.
+     *
+     * @param  string $key
+     * @param  mixed $value
+     * @return void
+     */
+    public function now($key, $value)
+    {
+        $this->put($key, $value);
+
+        $this->push('_flash.old', $key);
+    }
+
+    /**
+     * Reflash all of the session flash data.
+     *
+     * @return void
+     */
+    public function reflash()
+    {
+        $this->mergeNewFlashes($this->get('_flash.old', []));
+
+        $this->put('_flash.old', []);
+    }
+
+    /**
+     * Reflash a subset of the current flash data.
+     *
+     * @param  array|mixed  $keys
+     * @return void
+     */
+    public function keep($keys = null)
+    {
+        $this->mergeNewFlashes($keys = is_array($keys) ? $keys : func_get_args());
+
+        $this->removeFromOldFlashData($keys);
+    }
+
+    /**
+     * Merge new flash keys into the new flash array.
+     *
+     * @param  array  $keys
+     * @return void
+     */
+    protected function mergeNewFlashes(array $keys)
+    {
+        $values = array_unique(array_merge($this->get('_flash.new', []), $keys));
+
+        $this->put('_flash.new', $values);
+    }
+
+    /**
+     * Remove the given keys from the old flash data.
+     *
+     * @param  array  $keys
+     * @return void
+     */
+    protected function removeFromOldFlashData(array $keys)
+    {
+        $this->put('_flash.old', array_diff($this->get('_flash.old', []), $keys));
+    }
+
+    /**
+     * Flash an input array to the session.
+     *
+     * @param  array  $value
+     * @return void
+     */
+    public function flashInput(array $value)
+    {
+        $this->flash('_old_input', $value);
     }
 }
