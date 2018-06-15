246edd21b9ad21a115406031e37354feac6d5825
Bruno Goncalves <bugotech@gmail.com>
2018-06-15 05:20:25 -0300

    session do synfony

--- src/Session.php
+++ src/Session.php
@@ -1,6 +1,31 @@
 <?php namespace Nano7\Http;
 
+use Nano7\Foundation\Support\Arr;
+
 class Session extends \Symfony\Component\HttpFoundation\Session\Session
 {
+    /**
+     * Determine if the session contains old input.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasOldInput($key = null)
+    {
+        $old = $this->getOldInput($key);
 
+        return is_null($key) ? count($old) > 0 : ! is_null($old);
+    }
+
+    /**
+     * Get the requested item from the flashed input array.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function getOldInput($key = null, $default = null)
+    {
+        return Arr::get($this->get('_old_input', []), $key, $default);
+    }
 }
